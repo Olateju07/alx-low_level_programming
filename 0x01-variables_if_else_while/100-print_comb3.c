a2430052c01481a743ea6d5f4ce51b97ae35d7f7
Olateju07 <okeolatejujulius@gmail.com>
2022-04-11 19:17:11 -0700

    bug fixed

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -25,7 +25,7 @@ int main(void)
 			if (d != c && d < c)
 			{
 				putchar('0' + d);
-				putchar('0' + d);
+				putchar('0' + c);
 
 				if (c + d != 17)
 				{
@@ -33,7 +33,6 @@ int main(void)
 					putchar(' ');
 				}
 			}
-			
 			c++;
 		}
 		d++;
